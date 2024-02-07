448bdc1fe302f05d9670aec9b49d3550e6bad0be
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2024-02-07 10:20:52 +0100

    Initial commit

---
+++ src/main/java/com/example/demo_tutorial/HelloController.java
@@ -0,0 +1,41 @@
+package com.example.demo_tutorial;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/sum")
+    public String calculate(@RequestParam("number1") int number1, @RequestParam("number2") int number2, @RequestParam("operation") String operation) {
+
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = number1 + number2;
+                break;
+            case "-":
+                result = number1 - number2;
+                break;
+            case "*":
+                result = number1 * number2;
+                break;
+            case "/":
+                result = number1 / number2;
+                break;
+            default:
+                return "Invalid operation";
+        }
+
+        return String.format("Result: " + result);
+    }
+}
+
+
